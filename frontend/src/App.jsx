4b88efb71f8f763018680a2b8d65623b508e305f
Therrmann <Timo.Herrmann.HH@gmx.de>
2026-04-11 21:14:29 +0200

    Test Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import TeamCreate from './TeamCreate'
+import Settings from './Settings'
+import StartGame from './StartGame'
+import GameMain from './GameMain'
+import { GameProvider } from './GameContext'
+import Auth from './Auth'
+import Admin from './Admin'
+
+export default function App(){
+  return (
+    <GameProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/team-create" element={<TeamCreate/>} />
+          <Route path="/settings" element={<Settings/>} />
+          <Route path="/start-game" element={<StartGame/>} />
+          <Route path="/game" element={<GameMain/>} />
+          <Route path="/join-team" element={<div style={{padding:20}}><h2>Team Beitreten</h2><p>Feature folgt.</p></div>} />
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/admin" element={<Admin/>} />
+        </Routes>
+      </BrowserRouter>
+    </GameProvider>
+  )
+}
